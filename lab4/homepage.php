51eb0c1d9e7b962137af36e80790fee382baedea
D M Shafin Ahmed <84924987+ShafinAhmedd@users.noreply.github.com>
2021-06-29 21:05:28 +0600

    Add files via upload
    task 4 done

---
+++ lab4/homepage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Sports Club</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+  </head>
+  <body style="background-color:rgb(204, 255, 204);"> 
+<div class="container" id="container" style='border:2px black solid;'>
+<div class="menu" style="font-size:18px; text-align: right;">
+<?php include 'menu.php';?>
+</div> 
+<a href="homepage.php" ><img  src="logo.jpg" alt="logo" width="250px" height="160px"></a>
+<h1 align="" style="text-align: center;">Welcome To Sports Club!</h1>
+</div>
+
+</body>
+</html>
