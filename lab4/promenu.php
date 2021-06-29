47d1d32057f8021c7bc2204aaec17c8822eb0072
D M Shafin Ahmed <84924987+ShafinAhmedd@users.noreply.github.com>
2021-06-29 21:03:48 +0600

    Delete lab4 directory

--- lab4/promenu.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '<a href="#">Dashboard</a> <br>
-<a href="load.php">View Profile</a> <br>
-<a href="store.php">Edit Profile</a> <br>
-<a href="File.php">Change Profile Picture</a> <br>
-<a href="chgpass.php">Change Password</a> <br>
-<a href="logout.php">Logout</a> <br>';
-?>
